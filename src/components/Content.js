fd37c36bf50f9373353f192fae51305ed38b8cdd
Akash Sharma <akashsharma.zak@gmail.com>
2021-09-27 17:20:04 +0530

    Created Part Component of Content Comp

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Part from "./Part";
+
+const Content = ({content}) => {
+    console.log(content);
+    return(
+        <div>
+            {content.map(
+                c =>
+                <Part key={c.id} name={c.name} exercise={c.exercises} 
+                />)}
+        </div>
+    )
+};
+
+export default Content;
