60e916e55e32092a0d232942f471a77d3370a8a3
Akash Sharma <akashsharma.zak@gmail.com>
2021-09-27 17:03:59 +0530

    Created Content Component

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Content from './Content';
 
 const Course = (props) => {
     const { course } = props;
@@ -7,6 +8,7 @@ const Course = (props) => {
     return(
         <div>
             <Header header={course.name} />
+            <Content content={course.parts} />
         </div>
     )
 };
